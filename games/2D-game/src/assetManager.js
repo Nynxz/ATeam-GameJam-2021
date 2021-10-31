8454a54ac5e67007a36f4d62236718e3579d5fcc
Nynxz <nynxzx@gmail.com>
2021-10-31 17:40:34 +1000

    Add Assets

--- games/2D-game/src/assetManager.js
+++ games/2D-game/src/assetManager.js
@@ -3,6 +3,9 @@ class AsssetManager {
         buttons: {
             start: null,
             mapEditor: null,
+            newMapButton: null,
+            loadMapButton: null,
+            exitButton: null,
             back: null
         }
     }
@@ -10,6 +13,10 @@ class AsssetManager {
     static loadAssets() {
         AsssetManager.assets.buttons.start = loadImage("assets/buttons/startButton.png")
         AsssetManager.assets.buttons.mapEditor = loadImage("assets/buttons/mapEditorButton.png")
+        AsssetManager.assets.buttons.newMapButton = loadImage("assets/buttons/newMapButton.png")
+        AsssetManager.assets.buttons.loadMapButton = loadImage("assets/buttons/loadMapButton.png")
+        AsssetManager.assets.buttons.exitButton = loadImage("assets/buttons/exitButton.png")
         AsssetManager.assets.buttons.back = loadImage("assets/buttons/backButton.png")
+        
     }
 }
